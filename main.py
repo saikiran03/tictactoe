582fcc976f36537e3a293073553d741393427a10
Sai Kiran <saikiraniitkgp7@gmail.com>
2017-03-20 00:56:22 +0530

    show current player

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from minimax import minimax
 if __name__=='__main__':
 
     t = tictactoe()
+    player = ["You", "Comp"]
+    print t
+
     for it in xrange(9):
 
         if it%2==0:
@@ -12,17 +15,15 @@ if __name__=='__main__':
                     i, j = map(int, raw_input("Enter a move in the format i j : ").split())
                     if t.mark(i, j): break
                 except:
-                    pass
+                    print "Enter a valid move."
 
         else:
             t = minimax(t, 0)
 
+        print player[it%2]
         print t
         if t.terminal_test():
-            if t.win(1):
-                print "You Won (^.^)"
-            if t.win(0):
-                print "You Lose ('o')"
-            if t.draw():
-                print "It's a Draw (-_-)"
+            if t.win(1): print "You Won (^.^)"
+            if t.win(0): print "You Lose ('o')"
+            if t.draw(): print "It's a Draw (-_-)"
             break
